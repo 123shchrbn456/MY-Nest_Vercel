1ed85ce7948ff7dc3dbd359a033b4d74cbdd8e1e
Igor <qwerty2244668800@gmail.com>
2024-06-20 11:16:45 +0300

    Car and Driver resources are added

---
+++ src/driver/driver.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateDriverDto } from './dto/create-driver.dto';
+import { UpdateDriverDto } from './dto/update-driver.dto';
+import { PrismaService } from '../prisma.service';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class DriverService {
+  constructor(private prisma: PrismaService) {}
+
+  create(dto: Prisma.DriverCreateInput) {
+    return this.prisma.driver.create({
+      data: dto,
+    });
+  }
+
+  findAll() {
+    return this.prisma.driver.findMany();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} driver`;
+  }
+
+  update(id: number, updateDriverDto: UpdateDriverDto) {
+    return `This action updates a #${id} driver`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} driver`;
+  }
+}
